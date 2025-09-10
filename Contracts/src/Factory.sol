57120f9a9d3317d9155738ebb818861557491421
Ajuzie Sinachi <Ajuziesinachi@gmail.com>
2025-09-10 12:50:29 +0100

    feat: Wallet factory  - instantiating system contract address in consctructor. Wallet contract - setSchedule function for cron function, implementing cron functionality with reactive lib, emititng receive event for reaction implementation

--- Contracts/src/Factory.sol
+++ Contracts/src/Factory.sol
@@ -5,11 +5,14 @@ import {Ownable} from "../lib/openzeppelin-contracts/contracts/access/Ownable.so
 import {ReactWallet} from "./Wallet.sol";
 
 contract ReactWalletFactory is Ownable {
+    address public serviceAddress;
     mapping (address => address) public wallet;
-    constructor() Ownable(msg.sender) {}
+    constructor(address _serviceAddress) Ownable(msg.sender) {
+        serviceAddress = _serviceAddress;
+    }
 
     function createWallet() public onlyOwner {
-        wallet[msg.sender] = address(new ReactWallet(msg.sender));
+        wallet[msg.sender] = address(new ReactWallet(serviceAddress));
     }
 
     function getWallet() public view returns (address) {
--- Contracts/src/Wallet.sol
+++ Contracts/src/Wallet.sol
@@ -1,36 +1,54 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import {Ownable} from "../lib/openzeppelin-contracts/contracts/access/Ownable.sol";
 import {IERC20} from "../lib/forge-std/src/interfaces/IERC20.sol";
+import '../lib/reactive-lib/src/interfaces/ISystemContract.sol';
+import '../lib/reactive-lib/src/abstract-base/AbstractPausableReactive.sol';
+contract ReactWallet is AbstractPausableReactive {
+    address public walletOwner;
+    uint256 public CRON_TOPIC;
+    uint256 public scheduledAmount;
+    address public scheduledRecipient;
+    address public scheduledToken;
 
-contract ReactWallet is Ownable {
+    uint64 private constant GAS_LIMIT = 1000000;
+    uint256 public lastCronBlock;
+
     address public receiveModule;
     uint256 public receivePercentage;
     mapping (address => uint256) public approvedModules;
     mapping (address => mapping (address => uint256)) public approvedTokenModules;
-    
-    constructor(address owner) Ownable(owner) {}
 
-    function approveModuleETH(address module, uint256 amount) public onlyOwner {
+    event Received(
+        address indexed origin,
+        address indexed sender,
+        uint256 indexed value
+    );
+    
+    constructor(address _service) payable {
+        walletOwner = tx.origin;
+        service = ISystemContract(payable(_service));
+    }
+
+    function approveModuleETH(address module, uint256 amount) public onlyWalletOwner {
         approvedModules[module] = amount;
     }
 
-    function approveReceiveModule(address module, uint256 percentage) public onlyOwner {
+    function approveReceiveModule(address module, uint256 percentage) public onlyWalletOwner {
         receiveModule = module;
         receivePercentage = percentage;
     }
 
-    function approveModuleToken(address module, address token, uint256 amount) public onlyOwner {
+    function approveModuleToken(address module, address token, uint256 amount) public onlyWalletOwner {
         approvedTokenModules[module][token] = amount;
     }
 
-    function sendEth(address to, uint256 amount) public onlyOwner {
+    function sendEth(address to, uint256 amount) public onlyWalletOwner {
         require(address(this).balance >= amount, "Insufficient balance");
         payable(to).transfer(amount);
     }
 
-    function sendToken(address token, address to, uint256 amount) public onlyOwner {
+    function sendToken(address token, address to, uint256 amount) public onlyWalletOwner {
         require(IERC20(token).balanceOf(address(this)) >= amount, "Insufficient balance");
         IERC20(token).transfer(to, amount);
     }
@@ -55,5 +73,51 @@ contract ReactWallet is Ownable {
         _;
     }
 
+    modifier onlyWalletOwner() {
+        require(msg.sender == walletOwner, 'Unauthorized');
+        _;
+    }
+
+    function setSchedule(uint256 _cronTopic, uint256 amount, address recipient, address token) external onlyWalletOwner {
+        CRON_TOPIC = _cronTopic;
+        scheduledAmount = amount;
+        scheduledRecipient = recipient;
+        scheduledToken = token;
+        if (!vm) {
+            service.subscribe(block.chainid, address(service), _cronTopic, REACTIVE_IGNORE, REACTIVE_IGNORE, REACTIVE_IGNORE);
+        }
+    }
+
+    function getPausableSubscriptions() internal view override returns (Subscription[] memory) {
+        Subscription[] memory result = new Subscription[](1);
+        result[0] = Subscription(
+            block.chainid,
+            address(service),
+            CRON_TOPIC,
+            REACTIVE_IGNORE,
+            REACTIVE_IGNORE,
+            REACTIVE_IGNORE
+        );
+        return result;
+    }
+
+    function react(LogRecord calldata log) external vmOnly {
+        if (log.topic_0 == CRON_TOPIC) {
+            lastCronBlock = block.number;
+            if (scheduledToken == address(0)) {
+                payable(scheduledRecipient).transfer(scheduledAmount);
+            } else {
+                IERC20(scheduledToken).transfer(scheduledRecipient, scheduledAmount);
+            }
+        }
+    }
+
+    receive() external payable override(AbstractPayer, IPayer) {
+        emit Received(
+            tx.origin,
+            msg.sender,
+            msg.value
+        );
+    }
     
 }
